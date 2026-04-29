866e6edc74c6d493a254c7966ae728ad253420d0
PinoyCoder29 <bicojayvee4@gmail.com>
2026-04-29 15:01:10 +0800

    updated design for login

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,51 +1,76 @@
+"use client";
+import { useState } from "react";
+
 export default function login() {
+  const [show, setShow] = useState(false);
   return (
-    <main
-      style={{
-        minHeight: "100vh",
-        background: "linear-gradient(135deg, #090a0a, #4e575a, #3d393d)",
-      }}
-    >
-      <div className="p-5">
+    <main>
+      <div className="p-5 text-light">
         <div className="container">
           <div className="row justify-content-center">
-            <div className="col-md-6 shadow rounded-4">
-              <h2 className="text-center mb-3 text-light">
-                Sign in with email
-              </h2>
-              <form action="">
-                <div className="mb-2">
-                  {/* email */}
-                  <div className="mb-3">
-                    <div className="input-group shadow">
-                      <span className="input-group-text">
-                        <i className="bi bi-envelope"></i>
-                      </span>
+            <div className="col-md-6 shadow bg-dark rounded-5 p-5">
+              <div className="text-center">
+                <h2 className="mt-4">Sign In</h2>
+                <h6>Welcome back</h6>
+                <p className="text-secondary">Login your Chat app Account</p>
+              </div>
 
-                      <input
-                        type="email"
-                        className="form-control"
-                        placeholder="Email"
-                      />
-                    </div>
-                    {/* password */}
-                  </div>
-                  <div className="mb-3 input-group">
-                    <i className="bi bi-lock input-group-text"></i>
+              <form action=" " className="">
+                {/* email */}
+                <div className="mb-2">
+                  <label htmlFor="" className="form-label">
+                    Email
+                  </label>
+                  <div className="input-group">
+                    <i className="bi bi-envelope input-group-text"></i>
                     <input
-                      type="password"
-                      placeholder="password"
-                      className="form-control shadow"
+                      type="email"
+                      placeholder="Example@gmail.com"
+                      className="form-control"
+                      required
                     />
                   </div>
-                  <div className="text-center ">
+                </div>
+                {/* password */}
+                <div className="mb-3">
+                  <label htmlFor="">Password</label>
+                  <div className="input-group">
+                    <i className="bi bi-lock  input-group-text"></i>
+                    <input
+                      type={show ? "text" : "password"}
+                      className="form-control"
+                      placeholder="*********"
+                    />
                     <button
-                      className="btn btn-light text-center px-5"
-                      type="submit"
+                      type="button"
+                      className="input-group-text"
+                      onClick={() => setShow(!show)}
                     >
-                      Get Started
+                      <i className={show ? "bi bi-eye-slash" : "bi bi-eye"}></i>
                     </button>
                   </div>
+                </div>
+
+                <div className="mb-2 text-center">
+                  <button
+                    className="btn text-light w-100 rounded-5"
+                    type="button"
+                    style={{
+                      background:
+                        "linear-gradient(154deg,rgb(197, 31, 183),rgb(121, 29, 182),rgb(54, 78, 218))",
+                    }}
+                  >
+                    Sign In
+                  </button>
+                </div>
+
+                <div className="mb-3 text-end">
+                  <span className="">
+                    Don't have an account?
+                    <a href="/auth/signup" className="text-decoration-none">
+                      Sign Up
+                    </a>
+                  </span>
                 </div>
               </form>
             </div>
